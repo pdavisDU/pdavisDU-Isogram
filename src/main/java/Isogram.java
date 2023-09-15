8db3302751f5c072c0b6313d457985bb8c420748
Paul Davis <pdavisDU@gmail.com>
2023-09-15 21:27:32 +0000

    added and passed  testing

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,14 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        Set<Character> charSet = new HashSet<>();
+
+        for (char c : str.toCharArray()) {
+            if (charSet.contains(c)) {
+                return false;
+            }
+            charSet.add(c);
+        }
+        return true;
     }
 }
